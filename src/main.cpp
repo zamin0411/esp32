2af494984a8521bfa24026865dff0846e8e51694
shio0411 <giamanho37@gmail.com>
2022-10-17 15:34:20 +0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,86 @@
+  /**
+   ESP32 + DHT22 Example for Wokwi
+   
+   https://wokwi.com/arduino/projects/322410731508073042
+*/
+
+#include "WiFi.h"
+#include <HTTPClient.h>
+#include <Arduino_JSON.h>
+
+const char* serverName = "http://httpbin.org/get";
+
+unsigned long lastTime = 0;
+
+unsigned long timerDelay = 1000;
+
+String sensorReadings;
+float sensorReadingsArr[3];
+
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+    
+  // Your Domain name with URL path or IP address with path
+  http.begin(client, serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "{}"; 
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print("Connecting to WiFi");
+  WiFi.begin("Wokwi-GUEST", "", 6);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println(" Connected!");
+}
+
+
+void loop() {
+  //Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){              
+      sensorReadings = httpGETRequest(serverName);
+      Serial.println(sensorReadings);
+      JSONVar myObject = JSON.parse(sensorReadings);
+  
+      // JSON.typeof(jsonVar) can be used to get the type of the var
+      if (JSON.typeof(myObject) == "undefined") {
+        Serial.println("Parsing input failed!");
+        return;
+      }
+    
+      Serial.print("JSON object = ");
+      Serial.println(myObject);
+    
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
+
+
+
